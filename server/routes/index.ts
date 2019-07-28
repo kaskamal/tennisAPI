70c7b6456fd285fb738fb426ef2e4c19f44e5f52
kaskamal <kasra.kamal@gmail.com>
2019-07-27 23:01:13 -0700

    Added initial frontend display

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,12 +1,22 @@
 import * as express from 'express';
 import rankings from './rankings';
+import fs = require('fs');
 
 const router = express.Router();
 
 export default router;
 
 router.get("/", (req, res) => {
-    res.status(200).json({message: 'Connected'})
+    const path = "public/html/index.html";
+    fs.readFile(path, (err: Error, file: Buffer) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.write(file);
+            res.end();
+        }
+        
+    });
 })
 
 
